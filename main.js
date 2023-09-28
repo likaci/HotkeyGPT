0a243cd436bd83cefe049cb6b665793bb29a3b93
likaci <3407980+likaci@users.noreply.github.com>
2023-09-28 15:20:01 -0400

    support multi page

--- main.js
+++ main.js
@@ -1,39 +1,99 @@
-const { app, globalShortcut, BrowserWindow, clipboard } = require("electron");
+const {
+  app,
+  globalShortcut,
+  BrowserWindow,
+  BrowserView,
+  clipboard,
+} = require("electron");
 const path = require("path");
 
 let config = {
-  url: "https://chat.openai.com/",
-  hotkey: "alt+z",
-  prompt: "Explain to me in simple terms:\n",
-  autoSend: true,
+  pages: [
+    {
+      url: "https://chat.openai.com/",
+      hotkey: "alt+z",
+      prompt: "请用简单的语言解释给我:\n",
+      autoSend: true,
+    },
+    {
+      url: "https://chat.openai.com/",
+      hotkey: "alt+x",
+      prompt: "请把下面的句子中英互译, 并每句中英对照:\n",
+      autoSend: true,
+    },
+    {
+      url: "https://chat.openai.com/",
+      hotkey: "alt+c",
+      prompt: "你是一名优秀的软件工程师:\n",
+      autoSend: false,
+    },
+  ],
 };
 
 let mainWindow;
+let browserViews = [];
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
+    // width: 800,
+    // height: 600,
+    // webPreferences: {
+    //   preload: path.join(__dirname, "preload.js"),
+    // },
   });
+}
 
-  mainWindow.loadURL(config.url);
+function createPages() {
+  config.pages.forEach((page) => {
+    const browserView = new BrowserView({
+      webPreferences: {
+        preload: path.join(__dirname, "preload.js"),
+      },
+    });
+    mainWindow.setBrowserView(browserView);
+    browserViews.push(browserView);
+    browserView.setBounds({
+      x: 0,
+      y: 36,
+      width: mainWindow.getBounds().width,
+      height: mainWindow.getBounds().height - 36,
+    });
+    browserView.setAutoResize({
+      width: true,
+      height: true,
+      horizontal: false,
+      vertical: false,
+    });
+    browserView.webContents.loadURL(page.url);
+
+    globalShortcut.register(page.hotkey, () => {
+      mainWindow.show();
+      mainWindow.setBrowserView(browserView);
+      browserView.setBounds({
+        x: 0,
+        y: 36,
+        width: mainWindow.getBounds().width,
+        height: mainWindow.getBounds().height - 36,
+      });
+      browserView.setAutoResize({
+        width: true,
+        height: true,
+        horizontal: false,
+        vertical: false,
+      });
+      browserView.webContents.send("send-to-gpt", {
+        prompt: page.prompt,
+        autoSend: page.autoSend,
+        text: clipboard.readText(),
+      });
+    });
+  });
 }
 
 app.on("ready", () => {
   console.log("main.js", "ready");
   createWindow();
-
-  globalShortcut.register(config.hotkey, () => {
-    mainWindow.show();
-    mainWindow.webContents.send("send-to-gpt", {
-      prompt: config.prompt,
-      autoSend: config.autoSend,
-      text: clipboard.readText(),
-    });
-  });
+  createPages();
 
   app.on("will-quit", () => {
     console.log("main.js", "will-quit");
